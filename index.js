952b429a9caafac9bd5302aaf4897444d0706f95
abhijithpr123 <abhipr222@gmail.com>
2025-10-08 13:20:36 +0530

    number adjusted

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-setInterval(setClock,1000)
+setInterval(setClock, 1000)
 
 let hourHand = document.getElementById("hour");
 let minHand = document.getElementById("min");
 let secHand = document.getElementById("sec");
 
-function setClock(){
+function setClock() {
     let currentDate = new Date();
-    let secRatio=currentDate.getSeconds() / 60;
-    let minRatio=(secRatio + currentDate.getMinutes()) / 60;
-    let hourRatio=(minRatio+ currentDate.getHours()) / 12;
+    let secRatio = currentDate.getSeconds() / 60;
+    let minRatio = (secRatio + currentDate.getMinutes()) / 60;
+    let hourRatio = (minRatio + currentDate.getHours()) / 12;
 
-    setRotation(secHand , secRatio);
-    setRotation(minHand , minRatio);
-    setRotation(hourHand , hourRatio);
+    setRotation(secHand, secRatio);
+    setRotation(minHand, minRatio);
+    setRotation(hourHand, hourRatio);
 }
 
-function setRotation(e,r){
-    e.style.setProperty('--rotation',r * 360);
+function setRotation(e, r) {
+    e.style.setProperty('--rotation', r * 360);
 }
 
 setClock()
