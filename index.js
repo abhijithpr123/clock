437c1f1053b891a0dfd54f81e8177570d91ee60a
abhijithpr123 <abhipr222@gmail.com>
2025-10-13 12:08:55 +0530

    digital clock added

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-setInterval(setClock, 1000)
+setInterval(setClock, 1000);
 
 let hourHand = document.getElementById("hour");
 let minHand = document.getElementById("min");
 let secHand = document.getElementById("sec");
+let digitalHand = document.getElementById("digital");
 
 function setClock() {
     let currentDate = new Date();
-    let secRatio = currentDate.getSeconds() / 60;
-    let minRatio = (secRatio + currentDate.getMinutes()) / 60;
-    let hourRatio = (minRatio + currentDate.getHours()) / 12;
+
+    let seconds = currentDate.getSeconds();
+    let minutes = currentDate.getMinutes();
+    let hours = currentDate.getHours();
+
+    let secRatio = seconds / 60;
+    let minRatio = (seconds / 60 + minutes) / 60;
+    let hourRatio = (minRatio + hours) / 12;
 
     setRotation(secHand, secRatio);
     setRotation(minHand, minRatio);
     setRotation(hourHand, hourRatio);
+
+    updateDigitalClock(hours, minutes, seconds);
 }
 
-function setRotation(e, r) {
-    e.style.setProperty('--rotation', r * 360);
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
 }
 
-setClock()
+function updateDigitalClock(hours, minutes, seconds) {
+    let setDigital = (n) => (n < 10 ? "0" + n : n);
+    let ampm = hours >= 12 ? "PM" : "AM";
+    let displayHour = hours % 12 || 12;
+
+    digitalHand.textContent = `${setDigital(displayHour)}:${setDigital(minutes)}:${setDigital(seconds)} ${ampm}`;
+}
+
+setClock();
